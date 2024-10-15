92995334e71f024ae5a6149d87bc8d3309b1ccb9
IKNCODE <ajkaen863@gmail.com>
2024-10-15 21:47:38 +0300

    add new schemas

--- store/schemas.py
+++ store/schemas.py
@@ -1,12 +1,13 @@
 from pydantic import BaseModel, Field
-from models import Units, Products
+from models import Units, Products, Category
 
 class Unit(BaseModel):
     unit_name: str
 
 
 class Warehouses(BaseModel):
-    location: str
+    longitude: float
+    latitude: float
 
 class WarehouseResponse(BaseModel):
     Warehouse: Warehouses = Field()
@@ -14,12 +15,30 @@ class WarehouseResponse(BaseModel):
 class UnitResponse(BaseModel):
     Units: Unit = Field()
 
+class Categories(BaseModel):
+    name: str
+    descr: str
+
+class CategoriesResponse(BaseModel):
+    Category: Categories = Field()
+
+class CategoryResponse(BaseModel):
+    CategoryResponse: Categories = Field()
+
 class ProductsCreate(BaseModel):
     name: str
     unit: int
     warehouse_id: int
     count: int
     price: int
+    description: str
+    carb: int
+    protein: int
+    fats: int
+    calories: int
+    composition: str
+    store_condition: str
+    category_id: int
 
 class ProductsResponse(BaseModel):
     Products: ProductsCreate = Field()
