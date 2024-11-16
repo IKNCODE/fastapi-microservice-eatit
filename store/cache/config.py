437219bf6d96dbfefa44c1acd09eb59764b596c7
IKNCODE <ajkaen863@gmail.com>
2024-11-16 19:18:05 +0300

    add rabbitmq conf strings

--- store/cache/config.py
+++ store/cache/config.py
@@ -1,10 +1,7 @@
-import os
-
 import redis
 
-redis_host = os.environ.get('REDIS_HOST')
 
-r = redis.Redis(host=redis_host, port=6379, db=0)
+r = redis.Redis(host='redis', port=6379, db=0)
 
 TTL = 200
 LONG_TTL = 60 * 60 * 24 * 365
